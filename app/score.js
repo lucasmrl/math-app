b491db04fa0e38e08a946d8ece8aff8c45274d88
Lucas Lima <nutnewbr@gmail.com>
2024-04-17 23:37:45 -0500

    Added Score Screen

--- app/score.js
+++ app/score.js
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Link } from "expo-router";
 import colors from "../theme/colors/colors";
 import Header from "../components/Header";
 
@@ -6,6 +7,37 @@ export default function Score() {
   return (
     <View style={styles.container}>
       <Header title="Score" showSettingsIcon={false} />
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={styles.scorePercentage}>100%</Text>
+        <Text style={styles.scoreMsg}>Congrats!</Text>
+      </View>
+      <View
+        style={{
+          paddingBottom: 100,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Link href="/" asChild>
+          <TouchableOpacity style={styles.button}>
+            <Text
+              style={{
+                color: "#FFF",
+                fontFamily: "Lato_700Bold",
+                fontSize: 20,
+              }}
+            >
+              HOME
+            </Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
     </View>
   );
 }
@@ -14,5 +46,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.screenBackground,
+  },
+  scorePercentage: {
+    fontFamily: "Poppins_700Bold",
+    fontSize: 80,
+    paddingBottom: 4,
+  },
+  scoreMsg: {
+    fontFamily: "Poppins_700Bold",
+    fontSize: 40,
+    paddingBottom: 4,
+  },
+  button: {
+    alignItems: "center",
+    padding: 16,
+    width: "50%",
+    backgroundColor: "#36454F",
+    borderRadius: 5,
   },
 });
