c3d7ec6ad0011435dd9217a11ec33befcf0ba2c9
Lucas Lima <nutnewbr@gmail.com>
2024-04-24 11:31:03 -0500

    Working example of OCR

--- app/drawing.js
+++ app/drawing.js
@@ -1,86 +1,65 @@
 import React, { useRef, useState, useEffect } from "react";
-import { StyleSheet, SafeAreaView, Button } from "react-native";
+import { StyleSheet, SafeAreaView, Button, View } from "react-native";
 import { SketchCanvas, SketchCanvasRef } from "rn-perfect-sketch-canvas";
 import * as FileSystem from "expo-file-system";
 import MlkitOcr from "react-native-mlkit-ocr";
 import { btoa, atob } from "react-native-quick-base64";
+import {
+  Canvas,
+  Group,
+  RoundedRect,
+  Fill,
+  Path,
+  Circle,
+  Pat,
+  vec,
+} from "@shopify/react-native-skia";
 
 export default function Drawing() {
   const canvasRef = useRef();
-  const [result, setResult] = useState();
-
-  const base64ToArrayBuffer = (base64String) => {
-    const binaryString = atob(base64String);
-    const length = binaryString.length;
-    const bytes = new Uint8Array(length);
-    for (let i = 0; i < length; i++) {
-      bytes[i] = binaryString.charCodeAt(i);
-    }
-    return bytes.buffer;
-  };
-
-  useEffect(() => {
-    console.log(result);
-  }, [result]);
 
   const saveBase64Image = async (base64String, filename) => {
-    console.log(base64String);
-    const callback = (downloadProgress) => {
-      const progress =
-        downloadProgress.totalBytesWritten /
-        downloadProgress.totalBytesExpectedToWrite;
-      this.setState({
-        downloadProgress: progress,
-      });
-    };
-
     try {
-      const downloadResumable = FileSystem.createDownloadResumable(
-        "https://clipart-library.com/images/8cGbedjKi.jpg",
-        FileSystem.documentDirectory + "323.jpg",
-        {},
-        callback
-      );
+      const fileUri = FileSystem.cacheDirectory + filename;
+      await FileSystem.writeAsStringAsync(fileUri, base64String, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      console.log("Image saved successfully!" + fileUri);
+      const sample_image = `data:image/jpeg;base64,${base64String}`;
 
-      try {
-        const { uri } = await downloadResumable.downloadAsync();
-        console.log("Finished downloading to ", uri);
-        // Create file object
-        const fileUri = FileSystem.cacheDirectory + filename;
+      //   await new Promise((resolve) => setTimeout(resolve, 5000));
 
-        // Write base64 string to file
-        await FileSystem.writeAsStringAsync(fileUri, base64String, {
-          encoding: FileSystem.EncodingType.Base64,
-        });
-
-        // File saved successfully
-        console.log("Image saved successfully!");
-        setResult(await MlkitOcr.detectFromUri(uri));
-        return fileUri;
-      } catch (error) {
-        // Handle errors
-        console.error("Error saving image:", error);
-        return null;
-      }
-    } catch (e) {
-      console.error(e);
+      const resposta = await MlkitOcr.detectFromUri(sample_image);
+      console.log(resposta);
+      return "fileUri";
+    } catch (error) {
+      // Handle errors
+      console.error("Error saving image:", error);
+      return null;
     }
   };
+
+  const whiteContent = <View style={{ backgroundColor: "white", flex: 1 }} />;
 
   return (
     <SafeAreaView style={styles.container}>
       <SketchCanvas
         ref={canvasRef}
         strokeColor={"black"}
-        strokeWidth={8}
-        containerStyle={styles.container}
+        strokeWidth={20}
+        bottomChildren={
+          <Path path="M 0 0 H 200 V 380 H 0 L 0 0" color="white" />
+        }
+        containerStyle={styles.container2}
       />
       <Button onPress={canvasRef.current?.reset} title="Reset" />
       <Button
         onPress={() =>
-          saveBase64Image(
-            canvasRef.current.toImage().encodeToBase64(),
-            "test.png"
+          console.log(
+            saveBase64Image(
+              canvasRef.current.toImage().encodeToBase64(),
+              "myDesenho.jpg"
+            )
           )
         }
         title="Get Image"
@@ -92,5 +71,14 @@ export default function Drawing() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container2: {
+    width: "50%",
+    height: "50%",
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
---
+++ test.js
@@ -0,0 +1,8 @@
+[
+  {
+    bounding: { height: 225, left: 68, top: 108, width: 734 },
+    cornerPoints: [[Object], [Object], [Object], [Object]],
+    lines: [[Object]],
+    text: "SMILE",
+  },
+];
